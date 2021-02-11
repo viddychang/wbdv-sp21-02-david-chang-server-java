667299a0fd502b3dea450e40364bfb14c69205b1
dchang11194 <dchang11194@gmail.com>
2021-02-10 20:08:11 -0500

    main(), renderUsers(users), createUser() are done

--- src/main/webapp/admin/user-admin.controller.client.js
+++ src/main/webapp/admin/user-admin.controller.client.js
@@ -1,62 +1,101 @@
-(function () {
-    var userService = new UserService();
-    var rowTemplate;
-    var tbody;
-    var createUserBtn;
+(function (){
 
-    jQuery(main);
+    var $usernameFld, $passwordFld;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $userRowTemplate, $tbody;
+    var userService = new AdminUserServiceClient();
 
-    function main() {
-        rowTemplate = jQuery('.wbdv-template');
-        createUserBtn = jQuery('.wbdv-create');
-        tbody = jQuery('tbody');
-
-        createUserBtn.click(createUser);
-
-        userService
-            .findAllUsers()
-            .then(renderUsers)
-    }
+    var users = [
+/*        {username: "alice123", password: "password1", firstName: "Alice", lastName: "Park", role: "Admin" },
+        {username: "bob234", password: "password2", firstName: "Bob", lastName: "Costas", role: "Faculty" },
+        {username: "charlie345", password: "password3", firstName: "Charlie", lastName: "Conway", role: "Student" },
+        {username: "dan456", password: "password4", firstName: "Dan", lastName: "Orlovsky", role: "Student" }*/
+    ];
 
     function createUser() {
-        var usernameFld = $('#usernameFld');
-        var passwordFld = $('#passwordFld');
-        var firstNameFld = $('#firstNameFld');
-        var lastNameFld = $('#lastNameFld');
-        var roleFld = $('#roleFld');
-
-        var username = usernameFld.val();
-        var password = passwordFld.val();
-        var firstName = firstNameFld.val();
-        var lastName = lastNameFld.val();
-        var role = roleFld.val();
-
-
-        var user = {
-            username: username,
-            password: password,
-            firstName: firstName,
-            lastName: lastName,
-            role: role
+        var newUser = {
+            username: $usernameFld.val(),
+            password: $passwordFld.val(),
+            firstname: $firstNameFld.val(),
+            lastname: $lastNameFld.val(),
+            role: $roleFld.val()
         }
 
+        $usernameFld.val('')
+        $passwordFld.val('')
+        $firstNameFld.val('')
+        $lastNameFld.val('')
+        $roleFld.val('')
+
         userService
-            .createUser(user)
-            .then(renderUsers)
+            .createUser(newUser)
+            .then(function(actualUser) {
+                users.push(actualUser)
+                renderUsers(users)
+            })
     }
 
     function renderUsers(users) {
-        for(var u in users) {
-            const user = users[u]
-            const rowClone = rowTemplate.clone();
-            rowClone.removeClass('wbdv-hidden');
-            rowClone.find('.wbdv-username').html(user.username);
-            rowClone.find('.wbdv-first-name').html(user.firstName);
-            rowClone.find('.wbdv-last-name').html(user.lastName);
-            rowClone.find('.wbdv-role').html(user.role);
+        console.log(users)
+        $tbody.empty()
+        for (var i=0; i < users.length; i++) {
+            var user = users[i]
+            $tbody
+                .append(`
+                    <tr class= "wbdv-user">
+                        <td class="wbdv-username">${user.username}</td>
+                        <td>&nbsp;</td>
+                        <td class="wbdv-first-name">${user.firstname}</td>
+                        <td class="wbdv-last-name">${user.lastname}</td>
+                        <td class="wbdv-role">${user.role}</td>
+                        <td class="wbdv-actions">
+                    <span class="pull-right">
+                      <i id="${i}"class="fa-2x fa fa-times wbdv-remove"></i>
+                      <i id="${user._id}" class="fa-2x fa fa-pencil wbdv-edit"></i>
+                    </span>
+                        </td>
+                    </tr>
+                `)
 
-
-            tbody.append(rowClone);
         }
+        $removeBtn = $('.wbdv-remove')
+        $removeBtn.click(deleteUser)
+
+        $editBtn = $('.wbdv-edit')
+        $editBtn.click(selectUser)
+
     }
+
+
+
+
+    function main() {
+        $tbody = jQuery('.wbdv-tbody')
+        $removeBtn = $('.wbdv-remove')
+        $editBtn = $('.wbdv-update')
+        $createBtn = $('.wbdv-create')
+
+        $usernameFld = $('#usernameFld')
+        $passwordFld = $('#passwordFld')
+        $firstNameFld = $('#firstNameFld')
+        $lastNameFld = $('#lastNameFld')
+        $roleFld = $('#roleFld')
+
+        $createBtn.click(createUser)
+
+        userService
+            .findAllUsers()
+            .then(function(actualUsers) {
+                users = actualUsers
+                renderUsers(users)
+            })
+    }
+
+
+    $(main);
+
+
+
 })();
+
